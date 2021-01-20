fe574a984a283b647780b50d21727e7b4d170d04
Maydic <74528891+Maydic489@users.noreply.github.com>
2021-01-20 21:36:09 +0700

    Created a simple runner scene
    Created infinite empty road and player character and curved shader

---
+++ StardustRunner/Assets/StardustFolder/Scripts/SimpleLane.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+using MoreMountains.Tools;
+
+namespace MoreMountains.InfiniteRunnerEngine
+{
+	/// <summary>
+	/// Extends playable character to implement the specific gameplay of the Dragon level
+	/// </summary>
+	public class SimpleLane : PlayableCharacter
+	{
+		public float MoveSpeed = 5f;
+		public GameObject mainCamera;
+		private float slideDirection;
+		private char whatLane;
+
+        protected override void Start()
+        {
+			mainCamera = GameObject.Find("Main Camera");
+        }
+
+        protected override void Update()
+		{
+
+			// we determine the distance between the ground and the Jumper
+			ComputeDistanceToTheGround();
+			// we send our various states to the animator.      
+			UpdateAnimator();
+			// if we're supposed to reset the player's position, we lerp its position to its initial position
+			ResetPosition();
+			// we check if the player is out of the death bounds or not
+			CheckDeathConditions();
+
+			if(transform.position.x > -1f && transform.position.x < 1f)
+            {
+				whatLane = 'm';
+            }
+			else if(transform.position.x < 0)
+            {
+				whatLane = 'l';
+            }
+			else if(transform.position.x > 0)
+            {
+				whatLane = 'r';
+            }
+
+			if(transform.position.x != slideDirection)
+			transform.position = Vector3.MoveTowards(transform.position, new Vector3(slideDirection, transform.position.y, transform.position.z), MoveSpeed * Time.deltaTime);
+			mainCamera.transform.position = Vector3.MoveTowards(mainCamera.transform.position, new Vector3(slideDirection*0.8f, mainCamera.transform.position.y, mainCamera.transform.position.z), (MoveSpeed*0.8f) * Time.deltaTime);
+		}
+
+		public override void LeftStart()
+		{
+			//_rigidbodyInterface.AddForce(Vector3.left * MoveSpeed * Time.deltaTime);
+			if (whatLane == 'r')
+			{
+				slideDirection = 0f;
+			}
+			else
+            {
+				slideDirection = -1.5f;
+            }
+		}
+
+		public override void RightStart()
+		{
+			//_rigidbodyInterface.AddForce(Vector3.right * MoveSpeed * Time.deltaTime);
+			if (whatLane == 'l')
+			{
+				slideDirection = 0f;
+			}
+			else
+			{
+				slideDirection = 1.5f;
+			}
+		}
+	}
+}
---
+++ StardustRunner/Assets/StardustFolder/Scripts/WorldCurver.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[ExecuteInEditMode]
+public class WorldCurver : MonoBehaviour
+{
+	[Range(-0.1f, 0.1f)]
+	public float curveStrengthY = 0.01f;
+	[Range(-0.1f, 0.1f)]
+	public float curveStrengthX = 0.0f;
+    public bool isTurning;
+
+    int m_CurveStrengthID_Y;
+	int m_CurveStrengthID_X;
+
+    private int counting;
+    private float ranNum;
+    private float slideDirection;
+
+    private void OnEnable()
+    {
+        m_CurveStrengthID_Y = Shader.PropertyToID("_CurveStrengthY");
+		m_CurveStrengthID_X = Shader.PropertyToID("_CurveStrengthX");
+	}
+
+	void Update()
+	{
+		Shader.SetGlobalFloat(m_CurveStrengthID_Y, curveStrengthY);
+		Shader.SetGlobalFloat(m_CurveStrengthID_X, curveStrengthX);
+
+        //randomly curve road with shader
+        if (isTurning)
+        {
+            counting++;
+            if (counting == 300)
+            {
+                GetSlideDirection();
+                counting = 0;
+            }
+
+            curveStrengthX = Mathf.Lerp(curveStrengthX, slideDirection, 0.01f);
+        }
+    }
+
+    private void GetSlideDirection()
+    {
+        ranNum = Random.Range(0, 4);
+        if (ranNum <= 1)
+        {
+            slideDirection = -0.01f;
+        }
+        else if (ranNum <= 2)
+        {
+            slideDirection = 0;
+        }
+        else if (ranNum <= 3)
+        {
+            slideDirection = 0.01f;
+        }
+    }
+}
