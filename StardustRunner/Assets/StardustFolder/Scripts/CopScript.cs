34edbab4df7a4f4254bbc795ea19cf8ed2a4e46d
Maydic <74528891+Maydic489@users.noreply.github.com>
2021-02-01 23:32:55 +0700

    Cop move closer earlier but kill time still the same

--- StardustRunner/Assets/StardustFolder/Scripts/CopScript.cs
+++ StardustRunner/Assets/StardustFolder/Scripts/CopScript.cs
@@ -17,8 +17,17 @@ namespace MoreMountains.InfiniteRunnerEngine
         // Update is called once per frame
         void Update()
         {
-			transform.position = Vector3.MoveTowards(transform.position, new Vector3(SimpleLane.playerPositoin.x, SimpleLane.playerPositoin.y,
-				SimpleLane.playerPositoin.z - 1f - (GameManager.Instance.FuelPoints/10)), MoveSpeed * Time.deltaTime);
+            if (GameManager.Instance.FuelPoints > 50 || GameManager.Instance.FuelPoints < 5)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, new Vector3(SimpleLane.playerPositoin.x, SimpleLane.playerPositoin.y,
+                    SimpleLane.playerPositoin.z - 1f - (GameManager.Instance.FuelPoints / 10)), MoveSpeed * Time.deltaTime);
+            }
+            else
+            {
+                transform.position = Vector3.MoveTowards(transform.position, new Vector3(SimpleLane.playerPositoin.x, SimpleLane.playerPositoin.y,
+                   -1.21f), MoveSpeed * Time.deltaTime);
+            }
+            
 
             if (transform.position.z >= -1.2f && !anim.GetBool("isThere"))
                 anim.SetBool("isThere", true);
