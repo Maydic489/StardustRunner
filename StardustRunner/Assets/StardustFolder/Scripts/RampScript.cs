4ef8a2ef57991cf476ad42b649b0ebd1ad8c012b
Maydic <74528891+Maydic489@users.noreply.github.com>
2021-02-22 07:04:40 +0700

    add ramp and on/off shadow

---
+++ StardustRunner/Assets/StardustFolder/Scripts/RampScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MoreMountains.InfiniteRunnerEngine
+{
+	public class RampScript : MonoBehaviour
+	{
+		private bool isJump;
+
+        private void OnEnable()
+        {
+			isJump = false;
+        }
+        private void OnCollisionEnter(Collision other)
+		{
+			if(!isJump)
+			TriggerEnter(other.gameObject);
+		}
+
+		protected virtual void TriggerEnter(GameObject collidingObject)
+		{
+			if (collidingObject.tag != "Player") { return; }
+			isJump = true;
+			if (!SimpleLane.isSpeed)
+				collidingObject.GetComponent<Rigidbody>().AddForce(new Vector3(0, 15, 0), ForceMode.Impulse);
+			else
+				collidingObject.GetComponent<Rigidbody>().AddForce(new Vector3(0, 18, 0), ForceMode.Impulse);
+		}
+	}
+}
---
+++ StardustRunner/Assets/StardustFolder/Scripts/ShadowScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadowScript : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        TriggerEnter(other.gameObject);
+    }
+
+    protected virtual void TriggerEnter(GameObject collidingObject)
+    {
+        if(collidingObject.tag == "Road")
+        {
+            this.gameObject.GetComponent<MeshRenderer>().enabled = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        TriggerExit(other.gameObject);
+    }
+
+    protected virtual void TriggerExit(GameObject collidingObject)
+    {
+        if (collidingObject.tag == "Ramp")
+        {
+            this.gameObject.GetComponent<MeshRenderer>().enabled = false;
+        }
+    }
+}
