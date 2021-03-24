0b9d9ca2b426b771c5d0858e3f0661917a04441d
Maydic <74528891+Maydic489@users.noreply.github.com>
2021-03-24 22:40:14 +0700

    more localization in main menu

--- StardustRunner/Assets/StardustFolder/Scripts/Localization.cs
+++ StardustRunner/Assets/StardustFolder/Scripts/Localization.cs
@@ -6,10 +6,24 @@ public static class Localization
 {
     static string thisLanguage;
 
+    //main menu
     public static string startButton = "Play";
     public static string tutorialButton = "Tutorial";
     public static string settingButton = "Setting";
     public static string creditsButton = "Credits";
+
+    //setting
+    public static string st_TextTitle;
+    public static string st_SFX;
+    public static string st_Music;
+    public static string st_Eng;
+    public static string st_Thai;
+    public static string st_Skoi;
+    public static string st_Boy;
+    public static string st_Girl;
+
+    //Credits
+    public static string cr_TextTitle;
 
     public static void GetLanguage()
     {
@@ -30,24 +44,66 @@ public static class Localization
 
     public static void SetEnglish()
     {
+        //main menu
         startButton = "Play";
         tutorialButton = "Tutorial";
         settingButton = "Setting";
         creditsButton = "Credits";
+
+        //setting
+        st_TextTitle = "Setting";
+        st_SFX = "Sound FX";
+        st_Music = "Music";
+        st_Eng = "English";
+        st_Thai = "Thai";
+        st_Skoi = "Skoi";
+        st_Boy = "Boy";
+        st_Girl = "Girl";
+
+        //credits
+        cr_TextTitle = "Credits";
     }
     public static void SetThai()
     {
+        //main menu
         startButton = "เล่นเลย";
         tutorialButton = "เล่นไงอะ?";
         settingButton = "ตั้งค่า";
         creditsButton = "เครดิต";
+
+        //setting
+        st_TextTitle = "ตั้งค่า";
+        st_SFX = "เสียงเอฟเฟค";
+        st_Music = "เสียงเพลง";
+        st_Eng = "อังกฤษ";
+        st_Thai = "ไทย";
+        st_Skoi = "สก๊อย";
+        st_Boy = "เด็กแว้น";
+        st_Girl = "สาวสก๊อย";
+
+        //credits
+        cr_TextTitle = "เครดิต";
     }
     public static void SetSkoi()
     {
+        //main menu
         startButton = "เฬ่นเร่ย";
         tutorialButton = "เฬ่ณงั๊ยอ่?";
         settingButton = "ฏั๊งค่๊";
         creditsButton = "เครฎิฏ";
+
+        //setting
+        st_TextTitle = "ฏั๊งค่๊";
+        st_SFX = "เษิ๊ญงเฮฟเฟค";
+        st_Music = "เษิ๊ญงเภลง";
+        st_Eng = "อังกหรือษ";
+        st_Thai = "ไธญ";
+        st_Skoi = "สก๊อย";
+        st_Boy = "เฎ็กเเว๊ณ";
+        st_Girl = "ษ๊วสก๊อย";
+
+        //credits
+        cr_TextTitle = "เครฎิฏ";
     }
 }
 
--- StardustRunner/Assets/StardustFolder/Scripts/MainMenu.cs
+++ StardustRunner/Assets/StardustFolder/Scripts/MainMenu.cs
@@ -8,10 +8,24 @@ using UnityEngine.SceneManagement;
 //for GUI
 public class MainMenu : MonoBehaviour
 {
+    //main menu
     public TextMeshProUGUI startButton;
     public TextMeshProUGUI tutorialButton;
     public TextMeshProUGUI settingButton;
     public TextMeshProUGUI creditsButton;
+
+    //setting
+    public TextMeshProUGUI st_TextTitle;
+    public TextMeshProUGUI st_SFX;
+    public TextMeshProUGUI st_Music;
+    public TextMeshProUGUI st_Eng;
+    public TextMeshProUGUI st_Thai;
+    public TextMeshProUGUI st_Skoi;
+    public TextMeshProUGUI st_Boy;
+    public TextMeshProUGUI st_Girl;
+
+    //Credits
+    public TextMeshProUGUI cr_TextTitle;
 
     void Start()
     {
@@ -32,5 +46,16 @@ public class MainMenu : MonoBehaviour
         tutorialButton.text = Localization.tutorialButton;
         settingButton.text = Localization.settingButton;
         creditsButton.text = Localization.creditsButton;
+
+        st_TextTitle.text = Localization.st_TextTitle;
+        st_SFX.text = Localization.st_SFX;
+        st_Music.text = Localization.st_Music;
+        st_Eng.text = Localization.st_Eng;
+        st_Thai.text = Localization.st_Thai;
+        st_Skoi.text = Localization.st_Skoi;
+        st_Boy.text = Localization.st_Boy;
+        st_Girl.text = Localization.st_Girl;
+
+        cr_TextTitle.text = Localization.cr_TextTitle;
     }
 }
