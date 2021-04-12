3c293997bf7e7678cc562b7274202f783e9f664e
Maydic <74528891+Maydic489@users.noreply.github.com>
2021-04-12 14:11:48 +0700

    adjust share picture to be wider for better display

--- StardustRunner/Assets/StardustFolder/Scripts/NativeScreenShotShare.cs
+++ StardustRunner/Assets/StardustFolder/Scripts/NativeScreenShotShare.cs
@@ -22,8 +22,8 @@ namespace MoreMountains.InfiniteRunnerEngine
 		{
 			yield return new WaitForEndOfFrame();
 
-			Texture2D ss = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
-			ss.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+			Texture2D ss = new Texture2D(Screen.width*3, Screen.height, TextureFormat.RGB24, false);
+			ss.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), Screen.width, 0);
 			ss.Apply();
 
 			string filePath = Path.Combine(Application.temporaryCachePath, "shared img.png");
